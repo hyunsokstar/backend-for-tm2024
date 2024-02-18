b78fa03b86169d35355d7ec89d48f3450d421083
hyunsokstar <hyunsokstar@gmail.com>
2024-02-18 09:28:39 +0900

    related skilnotes2

--- src/app.module.ts
+++ src/app.module.ts
@@ -55,7 +55,7 @@ import { RoadMapModel } from "./technotes/entities/roadMap.entity";
         BookMarksForSkilNoteModel,
         BookMarksForSkilNoteContentsModel
       ],
-      synchronize: true,
+      synchronize: false,
     }),
     ConfigModule.forRoot({
       isGlobal: true, // 어디서든 사용할 수 있도록 설정
